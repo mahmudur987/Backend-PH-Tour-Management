e00524792cdb2c2db05cf276033daca1fdea170d
mahmudur987 <108387570+mahmudur987@users.noreply.github.com>
2025-07-21 23:08:17 +0600

    Update tour.model.ts

--- src/modules/tour/tour.model.ts
+++ src/modules/tour/tour.model.ts
@@ -4,11 +4,7 @@ import { ITour } from "./tour.interface";
 const tourSchema = new Schema<ITour>(
   {
     title: { type: String, required: true },
-<<<<<<< HEAD
     slug: { type: String, unique: true },
-=======
-    slug: { type: String, required: true, unique: true },
->>>>>>> a8f352ed112d4816a66845392bd4d499c22a57e9
     description: { type: String },
     images: { type: [String], default: [] },
     location: { type: String },
@@ -36,44 +32,5 @@ const tourSchema = new Schema<ITour>(
     timestamps: true,
   }
 );
-<<<<<<< HEAD
-// Removed import of Tour to avoid merged declaration error
-
-tourSchema.pre("save", async function (next) {
-  if (this.isModified("title")) {
-    const baseSlug = this.title.toLowerCase().split(" ").join("-") + "-tour";
-    let slug = baseSlug;
-    let counter = 1;
-
-    while (await Tour.exists({ slug })) {
-      slug = `${baseSlug}-${counter++}`;
-    }
-
-    this.slug = slug;
-  }
-
-  next();
-});
-
-tourSchema.pre("findOneAndUpdate", async function (next) {
-  const update = this.getUpdate() as Partial<ITour>;
-
-  if (update?.title) {
-    const baseSlug = update.title.toLowerCase().split(" ").join("-") + "-Tour";
-    let slug = baseSlug;
-    let counter = 1;
-
-    while (await Tour.exists({ slug })) {
-      slug = `${baseSlug}-${counter++}`;
-    }
-
-    update.slug = slug;
-    this.setUpdate(update); // Apply the modified update object
-  }
-
-  next();
-});
-=======
->>>>>>> a8f352ed112d4816a66845392bd4d499c22a57e9
 
 export const Tour = model<ITour>("Tour", tourSchema);
